d0213a6227ffe0c9def6fc60bd79d762d2d3e5f6
VolodymyrBorshchuk <vovkavovka000@gmail.com>
2024-01-25 14:33:15 +0200

    contact schema created, getContact, createContact logic implemented

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,22 +1,33 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 
 const getContacts = asyncHandler(async (req, res) => {
-    console.log('sdgj')
-    res.status(200).json({ message: "Get all contacts" })
+    const contacts = await Contact.find();
+    res.status(200).json(contacts)
 });
 
 const createContact = asyncHandler(async (req, res) => {
     console.log('sdgj', req.body);
-    const { name, email, number } = req.body;
-    if (!name || !email || !body) {
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
         res.status(400);
         throw new Error("All fields are mandatory!")
     }
-    res.status(200).json({ message: "Create contact" })
+    const contact = await Contact.create({
+        name,
+        email,
+        phone
+    })
+    res.status(200).json(contact)
 });
 
 const getContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Get contact for ${req.params.id}` })
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+    res.status(200).json(contact)
 });
 
 const updateContact = asyncHandler(async (req, res) => {
