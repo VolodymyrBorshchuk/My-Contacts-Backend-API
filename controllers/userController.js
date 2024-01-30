48753e6fec4817c7215e2e2b186f09755c7ceef5
VolodymyrBorshchuk <vovkavovka000@gmail.com>
2024-01-30 14:50:49 +0200

    JSON Web Token Access

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
 const User = require('../models/userModel');
+const { response } = require("express");
 
 const registerUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
@@ -35,7 +37,27 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ mesage: "Login user" })
+    const { email, password } = req.body;
+    if (!email || !password) {
+        throw new Error("All fields are mandatory!");
+    }
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.name,
+                email: user.email,
+                id: user.id,
+            }
+        },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "1m" }
+        )
+        res.status(200).json({ accessToken })
+    } else {
+        res.status(401);
+        throw new Error("Password is not valid!")
+    }
 });
 
 const currentUser = asyncHandler(async (req, res) => {
